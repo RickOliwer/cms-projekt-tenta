0734c03520858d5e496b9cb9191e05fff9426951
RickOliwer <rickoliwer@gmail.com>
2021-11-25 12:03:52 +0100

    remove DS_Store

--- inc/contact-content.php
+++ inc/contact-content.php
@@ -2,19 +2,19 @@
 
 if(!function_exists('my_contact_title')){
 
-function my_contact_title(){
+    function my_contact_title(){
 
-    if(!function_exists('get_field')){
-        return;
+        if(!function_exists('get_field')){
+            return;
+        }
+
+        $title = get_field('contact_title', false, false);
+
+        
+        if(!empty($title)){
+            printf('<p>%s</p>',
+                    $title,
+        );
+        }
     }
-
-    $title = get_field('contact_title', false, false);
-
-    
-    if(!empty($title)){
-        printf('<p>%s</p>',
-                $title,
-    );
-    }
-}
 }
--- template-parts/about-page.php
+++ template-parts/about-page.php
@@ -1,1 +1,2 @@
 <p>Hello About</p>
+<h2>sjdbkfnsldmövä'dbö</h2>
