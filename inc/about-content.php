909c688149db8724920188521da0a5558187236c
Nikola <nikola.tomasovic@medieinstitutet.se>
2021-11-25 14:39:44 +0100

    added function title on about page

---
+++ inc/about-content.php
@@ -0,0 +1,20 @@
+<?php
+
+if(!function_exists('about_title')){
+
+    function about_title(){
+
+        if(!function_exists('get_field')){
+            return;
+        }
+
+        $title = get_field('about_title', false, false);
+
+
+        if(!empty($title)){
+            printf('<p>%s</p>',
+                    $title,
+        );
+        }
+    }
+    }
--- template-parts/about-page.php
+++ template-parts/about-page.php
@@ -1,1 +1,6 @@
 <p>Hello About</p>
+
+<div class="" style="color: blue;">
+    <?php about_title(); ?>
+
+</div>
